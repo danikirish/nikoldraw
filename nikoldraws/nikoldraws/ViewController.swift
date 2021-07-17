18ae10c839c125a09e2719b714850f412416fa2c
Daniyar Irishev <irishevdaniyar@gmail.com>
2021-07-17 11:57:15 +0100

    restart project

---
+++ nikoldraws/nikoldraws/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nikoldraws
+//
+//  Created by Daniyar Irishev on 08/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
